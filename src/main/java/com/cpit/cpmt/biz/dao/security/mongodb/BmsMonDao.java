3677ad55524b7029938e4ee5c52aa65ba298c855
cpithz_shangchuanxia <cpithz_shangchuanxia@potevio.com>
2020-08-31 16:36:02 +0800

    删除all

--- src/main/java/com/cpit/cpmt/biz/dao/security/mongodb/BmsMonDao.java
+++
@@ -1,9 +0,0 @@
-package com.cpit.cpmt.biz.dao.security.mongodb;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cpit.cpmt.dto.security.mongodb.BmsMon;
-
-public interface BmsMonDao extends MongoRepository<BmsMon, String>{
-
-}
